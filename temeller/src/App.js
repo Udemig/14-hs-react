b1b4433f51aba2133ffd1f78b5e62c186b27943e
Furkan Evin <furkanevin00@gmail.com>
2025-08-25 00:48:18 +0300

    update

--- temeller/src/App.js
+++ temeller/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Card from "./components/card";
+import Form from "./components/form";
 
 function App() {
+  const products = ["Telefon", "Tablet", "Bilgisayar", "T-Shirt", "Atlet", "Forma", "Mouse", "Klavye"];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* Bir component kullanmak için <Component /> syntaxı kullanırız */}
+      <Header />
+
+      <main>
+        <h1>Ürünler</h1>
+
+        <Form />
+
+        <section>
+          {/* {products.map((product, index) => (
+            <Card key={index} />
+          ))} */}
+
+          <Card name="Telefon" stock={10} />
+          <Card name="Bilgisayar" stock={0} />
+          <Card name="Tablet" stock={6} />
+          <Card name="Mouse" stock={0} />
+        </section>
+      </main>
+
+      <Footer />
     </div>
   );
 }
