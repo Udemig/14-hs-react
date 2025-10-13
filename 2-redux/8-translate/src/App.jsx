c4596a536b54873ca7d5bb84f047141e47fe9d35
furkanevin <furkanevin00@gmail.com>
2025-10-13 23:55:39 +0300

    translate app completed

--- 2-redux/8-translate/src/App.jsx
+++ 2-redux/8-translate/src/App.jsx
@@ -1,11 +1,20 @@
+import { useEffect } from "react";
 import Button from "./components/button";
 import Footer from "./components/footer";
 import Header from "./components/header";
 import History from "./components/history";
 import LanguageSelect from "./components/language-select";
 import TextContainer from "./components/text-container";
+import { useDispatch } from "react-redux";
+import { getLanguages } from "./redux/actions";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getLanguages());
+  }, []);
+
   return (
     <div className="bg-gradient-to-br from-zinc-900 via-zinc-800 to-zinc-900 min-h-screen text-white grid place-items-center">
       <div className="container mx-auto px-4 py-8">
--- 2-redux/8-translate/src/components/text-container.jsx
+++ 2-redux/8-translate/src/components/text-container.jsx
@@ -1,6 +1,62 @@
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Volume2 } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { setText } from "../redux/slices/translateSlice";
+import Loader from "./loader";
 
 const TextContainer = () => {
+  const { textToTranslate, translatedText, sourceLang, targetLang, isLoading } = useSelector(
+    (store) => store.translateReducer
+  );
+  const dispatch = useDispatch();
+
+  // metni temizle
+  const handleClear = () => {
+    dispatch(setText(""));
+  };
+
+  // metni kopyala
+  const handleCopy = () => {
+    window.navigator.clipboard.writeText(translatedText);
+  };
+
+  // çevrilcek metni seslendir
+  const handleSpeakSource = () => {
+    if (!textToTranslate) return;
+
+    // mevcut seslendirilen metni durdur
+    window.speechSynthesis.cancel();
+
+    // SpeechSynthesisUtterance: Seslendirelecek metni ve ayarlrını tutan bir nesne oluştur
+    // parametre olarak seslendirelecek metni alır
+    // Bu nesne üzerinde dil, ses tonu, hız gibi özellikler ayarlanabilir
+    const utterance = new SpeechSynthesisUtterance(textToTranslate);
+
+    // utterance.lang: hangi dilde/aksanda konuşulucağını belirler
+    // bu sayede metin doğru aksanla seslendirilecek
+    // utterance.pitch: ses tonu ayarlar (0-5 arası değer alır)
+    if (sourceLang.value) {
+      utterance.lang = sourceLang.value;
+      // utterance.pitch = 0.5;
+    }
+
+    // window.speechSynthesis.speak(): Oluşturulan utterance nesnesini seslendirmeye başlar
+    // tarayıcının ses sentezleme motorunu kullanarak sesli olarak okur
+    window.speechSynthesis.speak(utterance);
+  };
+
+  // çevrilmiş metni seslendir
+  const handleSpeakTarget = () => {
+    if (!translatedText) return;
+
+    window.speechSynthesis.cancel();
+
+    const utterance = new SpeechSynthesisUtterance(translatedText);
+
+    utterance.lang = targetLang.value;
+
+    window.speechSynthesis.speak(utterance);
+  };
+
   return (
     <div className="flex gap-4 mt-6 lg:gap-8 flex-col lg:flex-row">
       {/* Çevrilecek Metin */}
@@ -8,12 +64,33 @@ const TextContainer = () => {
         <div className="flex items-center justify-between mb-2">
           <label className="text-sm text-zinc-300">Çevrilecek Metin</label>
 
-          <button className="text-xs text-zinc-400 hover:text-zinc-200">Temizle</button>
+          {textToTranslate && (
+            <div className="flex items-center gap-3">
+              <button
+                disabled={!sourceLang.value}
+                onClick={handleSpeakSource}
+                className="text-xs text-zinc-400 hover:text-zinc-300 flex gap-2 disabled:text-zinc-500 disabled:hover:cursor-not-allowed"
+              >
+                <Volume2 className="size-4" />
+                Seslendir
+              </button>
+              <button onClick={handleClear} className="text-xs text-zinc-400 hover:text-zinc-200">
+                Temizle
+              </button>
+            </div>
+          )}
         </div>
 
         <div className="relative">
-          <textarea className="textarea" placeholder="Çevirmek istediğiniz metni buraya yazınız"></textarea>
-          <div className="absolute bottom-2 right-2 text-xs text-zinc-500 bg-zinc-100">100 karakter</div>
+          <textarea
+            value={textToTranslate}
+            className="textarea"
+            placeholder="Çevirmek istediğiniz metni buraya yazınız"
+            onChange={(e) => dispatch(setText(e.target.value))}
+          ></textarea>
+          <div className="absolute bottom-2 right-2 text-xs text-zinc-500 bg-zinc-100">
+            {textToTranslate.length} karakter
+          </div>
         </div>
       </div>
 
@@ -29,10 +106,30 @@ const TextContainer = () => {
         <div className="flex items-center justify-between mb-2">
           <label className="text-sm text-zinc-300">Çevrilmiş Metin</label>
 
-          <button className="text-xs text-zinc-400 hover:text-zinc-200">Kopyala</button>
+          {translatedText && (
+            <div className="flex items-center gap-3">
+              <button onClick={handleSpeakTarget} className="text-xs text-zinc-400 hover:text-zinc-300 flex gap-2">
+                <Volume2 className="size-4" />
+                Seslendir
+              </button>
+              <button onClick={handleCopy} className="text-xs text-zinc-400 hover:text-zinc-200">
+                Kopyala
+              </button>
+            </div>
+          )}
         </div>
 
-        <textarea className="textarea" disabled></textarea>
+        <div className="relative">
+          <textarea className="textarea text-gray-300" disabled value={translatedText}></textarea>
+
+          {isLoading && <Loader />}
+
+          {!isLoading && !translatedText && !textToTranslate && (
+            <div className="absolute inset-0 grid place-items-center">
+              <div className="text-zinc-500 text-sm">Çeviri bekleniyor</div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
