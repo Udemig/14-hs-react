3892fe183ab6a484f5e4392ec5d709aa03a21afa
furkanevin <furkanevin00@gmail.com>
2025-10-28 03:24:25 +0300

    lesson update

---
+++ 4-firebase/2-twitter/src/firebase/index.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCcRDtMAE-j-4uM9Xl4K2Vb-m-waYlkPKg",
+  authDomain: "hs-twitter-aef02.firebaseapp.com",
+  projectId: "hs-twitter-aef02",
+  storageBucket: "hs-twitter-aef02.firebasestorage.app",
+  messagingSenderId: "14895350569",
+  appId: "1:14895350569:web:c6265d511c1ce040666d38",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// auth referansını al
+export const auth = getAuth(app);
+
+// google sağlayıcının kurulumunu yap
+export const provider = new GoogleAuthProvider();
---
+++ 4-firebase/2-twitter/src/pages/login/index.jsx
@@ -0,0 +1,22 @@
+import GoogleButton from "../../components/login/google-button";
+import LoginForm from "../../components/login/login-form";
+
+const Login = () => {
+  return (
+    <div className="h-screen bg-dark text-white grid place-items-center px-4">
+      <div className="bg-black py-16 px-28 rounded-lg flex flex-col gap-10 sm:w-[80%] max-w-[550px]">
+        <div className="flex justify-center">
+          <img src="/x-logo.webp" alt="x" className="h-[60px]" />
+        </div>
+
+        <h1 className="text-xl md:text-2xl font-bold text-center">Twitter'a Giriş Yap</h1>
+
+        <GoogleButton />
+
+        <LoginForm />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
